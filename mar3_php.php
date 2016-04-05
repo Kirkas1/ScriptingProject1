82d5fcac2e91e2a22bdf2692a4747eff174da9bd
Kirkas1 <ikirk870@gmail.com>
2016-04-05 13:35:56 -0400

    Changed title

--- mar3_php.php
+++ mar3_php.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title> IS448 hw#2 9.9 Kayoung Kim  </title>
+<title>Project 1</title>
 <link rel = "stylesheet" href = "mar3_style.css" />
 </head>
 <body>
