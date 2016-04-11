55ac38f43df40257d992fabc138a31f444057867
asmith42 <alex42@umbc.edu>
2016-04-11 14:22:52 -0400

    Added php validation

--- right.php
+++ right.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -10,6 +9,61 @@
 
 <div class=""><center> WELCOME <span class="textstyleRed">COMPUTER SCIENCE</span> STUDENT</center>
 
+
+<?php
+
+if (!empty($_POST))
+{
+    
+
+	$name = $_POST["studentName"];
+	$CID = $_POST["campusID"];
+	$email = $_POST["email"];
+	$number = $_POST["contactNum"];
+
+	if(preg_match('/[A-Z][a-z]* [A-Z][a-z]*/', $name) == 1){
+		$name = test_input($name);
+	}
+	else{
+		echo "Please enter your first and last name<br>";
+	}
+
+	if(preg_match('/[A-Z][A-Z][0-9][0-9][0-9][0-9][0-9]/', $CID) == 1){
+		$CID = test_input($CID);
+	}
+	else{
+		echo "Please enter a valid campus ID<br>";
+	}
+
+	if(preg_match('/.*@.*\..*/', $email) == 1 ){
+		$email=trim($email);
+		$email=stripslashes($email);
+	}
+	else{
+		echo "Please enter a valid email<br>";
+	}
+	$number = str_replace('-', '', $number);
+	if(preg_match('/[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/', $number) == 1){
+		$number = test_input($number);
+	}
+	else{
+		echo "Please enter a valid phone number <br>";
+	}
+
+
+
+	echo "Name: $name<br>Campus ID: $CID<br>Email: $email<br>Phone Number: $number<br>";
+
+}
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+
+?>
 
 </body>
 </html>
