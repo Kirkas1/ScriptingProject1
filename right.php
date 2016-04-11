0e3b94edca54244df9f28560578fec652c13e588
Kirkas1 <ikirk870@gmail.com>
2016-04-11 14:15:58 -0400

    Added frame change

---
+++ right.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> IS448 hw#2 9.9 Kayoung Kim  </title>
+<link rel = "stylesheet" href = "aki_style.css" />
+</head>
+<body class="right">
+
+<div class=""><center> WELCOME <span class="textstyleRed">COMPUTER SCIENCE</span> STUDENT</center>
+
+
+</body>
+</html>
