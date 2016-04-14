40bde44b183ddb6cbbbae9941133857f96671c53
asmith42 <alex42@umbc.edu>
2016-04-14 18:34:28 -0400

    Revert "final commit (hopefully)"
    This reverts commit 501ddef794cd780dbbf1b7b7b728de230ea757b3.

--- right.php
+++ right.php
@@ -1,4 +1,3 @@
-<?php session_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//Dbr XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/Dbr/xhtml1-transitional.dbr">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -262,7 +261,7 @@ if (!empty($_POST))
 	    	while($row = $result->fetch_assoc()) {
 
 	    		// Checks if the current user's name or campus id matches any in the databse
-		        if ($name == $row["name"] || $campusID == $row["campusid"]) {
+		        if ($name == $row["name"] || $campusID == $row["campusID"]) {
 		        	$inDB = TRUE;
 		    	}
 		    }
@@ -297,7 +296,7 @@ if (!empty($_POST))
 		echo "<br><div><center> WELCOME <span class='textstyleRed'>$nameUpper</span></center><br>";
 		
 		// Saves session variables for the next page, after.php
-		
+		session_start();
 		$_SESSION["name"] = $name;
 		$_SESSION["campusID"] = $campusID;
 		$_SESSION["email"] = $email;
